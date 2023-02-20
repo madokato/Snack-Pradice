e4dcbb5af7c43bdfd8d7528bffda28ad91597134
Madoka Kato <maka7816@yahoo.co.jp>
2023-02-20 18:58:45 +0900

    supabase authの実装

--- src/api/login.js
+++ src/api/login.js
@@ -1,27 +1,38 @@
 import { supabase } from "../supabase";
+import router from "@/router";
 
 export const CreateAccount = async (val) => {
   console.log(val);
   //   await supabase.from("users").insert(val);
 
-  const { data, error } = await supabase.auth.signUp({
+  const { user,session,data, error } = await supabase.auth.signUp({
     email: val.eMail,
     password: val.passWord,
-    options: {
-      data: {
-        name: val.name,
-      },
-    },
+    // options: {
+    //   data: {
+    //     name: val.name,
+    //   },
+    // },
   });
+ 
 };
+
+
 export const Login = async (val) => {
+  const { data} = await supabase.auth.getUser()
+  console.log("besfore",data)
   try {
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error,user } = await supabase.auth.signInWithPassword({
       email: val.eMail,
       password: val.passWord,
     });
+    const { data} = await supabase.auth.getUser()
+  console.log(data)
     if (error) throw error;
-    console.log('login!')
+    // router.push({
+    //   name: 'home',
+    // });
+  
   } catch (error) {
     if (error instanceof Error) {
       //   alert(error.message);
